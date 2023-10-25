bc9574db69588b558a7ce058fe41be50c6e69759
Пользователь <world.worensi@gmail.com>
2023-10-25 16:40:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Выполнить (+, *, /, %)
+
+// let a = 4
+// let b = 55
+// console.log(a+b)
+// console.log(a-b);
+// console.log(a/b);
+// console.log(b%a)
+
+
+// вывести в консоль hello (ваше имя)
+// let a = 'hello'
+// let b ='Dastan'
+// console.log(a+' '+b)
+
+//вывести в консоль NaN
+// let a = 'hello'
+// let b = 3
+// console.log(a-b)
+
+//вывести в консоль 10
+// let a = 5
+// let b = '5'
+// console.log(+b+a)
+
+// let c = 2
+// let b = undefined
+// console.log(b)
